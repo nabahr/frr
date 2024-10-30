4ecb6fb0a5eda4e099195ad861fb2626c45a4fac
Nathan Bahr <nbahr@atcorp.com>
2024-10-30 21:16:30 +0000

    pimd,yang: Expand rpf-lookup-mode command
    Add options for group-list and source-list, both of which take a prefix list name.
    The prefix list is used to determine the lookup mode for specific sources and/or groups.
    Any number of lookup modes can be configured as long as the combination of group
    and source list is unique.
    A global lookup mode (empty group and source lists) is always added and defaults to mrib-then-urib
    as it currently functions. The global lookup mode can be changed as it current exists with the command
    `rpf-lookup-mode MODE`.
    When determinig which mode to use, match source (and group if provided) against the lists, if they are set.
    If a lookup does not specify a group, then only use lookup modes that do not have a group list defined.
    A lookup by definition will have a source, so no special handling there.

--- pimd/pim_nb_config.c
+++ pimd/pim_nb_config.c
@@ -1675,47 +1675,14 @@ int routing_control_plane_protocols_control_plane_protocol_pim_address_family_re
 /*
  * XPath: /frr-routing:routing/control-plane-protocols/control-plane-protocol/frr-pim:pim/address-family/mcast-rpf-lookup
  */
-int routing_control_plane_protocols_control_plane_protocol_pim_address_family_mcast_rpf_lookup_modify(
-	struct nb_cb_modify_args *args)
+int routing_control_plane_protocols_control_plane_protocol_pim_address_family_mcast_rpf_lookup_create(
+	struct nb_cb_create_args *args)
 {
-	struct vrf *vrf;
-	struct pim_instance *pim;
-	const char *mode;
-	enum pim_rpf_lookup_mode old_mode;
-
 	switch (args->event) {
 	case NB_EV_VALIDATE:
 	case NB_EV_PREPARE:
 	case NB_EV_ABORT:
-		break;
 	case NB_EV_APPLY:
-		vrf = nb_running_get_entry(args->dnode, NULL, true);
-		pim = vrf->info;
-		mode = yang_dnode_get_string(args->dnode, NULL);
-
-		old_mode = pim->rpf_mode;
-		if (strmatch(mode, "urib-only"))
-			pim->rpf_mode = MCAST_URIB_ONLY;
-		else if (strmatch(mode, "mrib-only"))
-			pim->rpf_mode = MCAST_MRIB_ONLY;
-		else if (strmatch(mode, "mrib-then-urib"))
-			pim->rpf_mode = MCAST_MIX_MRIB_FIRST;
-		else if (strmatch(mode, "lower-distance"))
-			pim->rpf_mode = MCAST_MIX_DISTANCE;
-		else if (strmatch(mode, "longer-prefix"))
-			pim->rpf_mode = MCAST_MIX_PFXLEN;
-		else {
-			snprintfrr(args->errmsg, args->errmsg_len,
-				   "Invalid RPF lookup mode specified: %s", mode);
-			return CMD_WARNING_CONFIG_FAILED;
-		}
-
-		if (pim->rpf_mode != old_mode &&
-		    /* MCAST_MIX_MRIB_FIRST is the default if not configured */
-		    (old_mode != MCAST_NO_CONFIG && pim->rpf_mode != MCAST_MIX_MRIB_FIRST)) {
-			pim_nht_mode_changed(pim);
-		}
-
 		break;
 	}
 
@@ -1727,7 +1694,6 @@ int routing_control_plane_protocols_control_plane_protocol_pim_address_family_mc
 {
 	struct vrf *vrf;
 	struct pim_instance *pim;
-	enum pim_rpf_lookup_mode old_mode;
 
 	switch (args->event) {
 	case NB_EV_VALIDATE:
@@ -1737,12 +1703,60 @@ int routing_control_plane_protocols_control_plane_protocol_pim_address_family_mc
 	case NB_EV_APPLY:
 		vrf = nb_running_get_entry(args->dnode, NULL, true);
 		pim = vrf->info;
-		old_mode = pim->rpf_mode;
-		pim->rpf_mode = MCAST_NO_CONFIG;
-		/* MCAST_MIX_MRIB_FIRST is the default if not configured */
-		if (old_mode != MCAST_NO_CONFIG && old_mode != MCAST_MIX_MRIB_FIRST) {
-			pim_nht_mode_changed(pim);
+		pim_nht_change_rpf_mode(pim, yang_dnode_get_string(args->dnode, "group-list"),
+					yang_dnode_get_string(args->dnode, "source-list"),
+					MCAST_NO_CONFIG);
+		break;
+	}
+
+	return NB_OK;
+}
+
+/*
+ * XPath: /frr-routing:routing/control-plane-protocols/control-plane-protocol/frr-pim:pim/address-family/mcast-rpf-lookup/mode
+ */
+int routing_control_plane_protocols_control_plane_protocol_pim_address_family_mcast_rpf_lookup_mode_modify(
+	struct nb_cb_modify_args *args)
+{
+	struct vrf *vrf;
+	struct pim_instance *pim;
+	const char *smode;
+	enum pim_rpf_lookup_mode mode = MCAST_NO_CONFIG;
+
+	smode = yang_dnode_get_string(args->dnode, NULL);
+
+	switch (args->event) {
+	case NB_EV_VALIDATE:
+		if (!(strmatch(smode, "urib-only") || strmatch(smode, "mrib-only") ||
+		      strmatch(smode, "mrib-then-urib") || strmatch(smode, "lower-distance") ||
+		      strmatch(smode, "longer-prefix"))) {
+			snprintfrr(args->errmsg, args->errmsg_len,
+				   "Invalid RPF lookup mode specified: %s", smode);
+			return CMD_WARNING_CONFIG_FAILED;
 		}
+		break;
+
+	case NB_EV_PREPARE:
+	case NB_EV_ABORT:
+		break;
+
+	case NB_EV_APPLY:
+		vrf = nb_running_get_entry(args->dnode, NULL, true);
+		pim = vrf->info;
+
+		if (strmatch(smode, "urib-only"))
+			mode = MCAST_URIB_ONLY;
+		else if (strmatch(smode, "mrib-only"))
+			mode = MCAST_MRIB_ONLY;
+		else if (strmatch(smode, "mrib-then-urib"))
+			mode = MCAST_MIX_MRIB_FIRST;
+		else if (strmatch(smode, "lower-distance"))
+			mode = MCAST_MIX_DISTANCE;
+		else if (strmatch(smode, "longer-prefix"))
+			mode = MCAST_MIX_PFXLEN;
+
+		pim_nht_change_rpf_mode(pim, yang_dnode_get_string(args->dnode, "../group-list"),
+					yang_dnode_get_string(args->dnode, "../source-list"), mode);
 		break;
 	}
 
